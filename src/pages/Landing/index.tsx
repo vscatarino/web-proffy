5c70a4514c3311c8bb2684104dafdd87831173a0
viniciuscatarino <vinicius.catarino@calindra.com.br>
2020-08-05 11:06:42 -0300

    estilizando Landing page - desktop

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -11,6 +11,7 @@ function Landing () {
  return (
   <div id="page-landing">
    <div id="page-landing-content" className="container">
+    
     <div className="logo-container">
      <img src={ logoImg } alt="Proffy" />
      <h2>Sua plataforma de estudos online.</h2>
@@ -20,16 +21,18 @@ function Landing () {
      src={ landingImg }
      alt="Plataforma de estudos"
      className="hero-image" />
+    
     <div className="buttons-container">
      <a href="" className="study">
       <img src={ studyIcon } alt="Estudar" />
       Estudar
      </a>
      <a href="" className="give-classes">
-      <img src={ giveClassesIcon } alt="Estudar" />
-      Estudar
+      <img src={ giveClassesIcon } alt="Dar aulas" />
+      Dar aulas
      </a>
     </div>
+
     <span className="total-connections">
      Total de 200 conexões já realizadas<img src={ purpleHeartIcon } alt="Coração roxo" />
     </span>
